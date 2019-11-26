da7d6d79a91c158a465b3001bb3553e253aa6c79
Luciano Alves <ti4luciano@gmail.com>
2019-11-25 23:05:47 -0300

    Property , ,

--- knockout/book_knockout_succinctly/viewModel.js
+++ knockout/book_knockout_succinctly/viewModel.js
@@ -1,8 +1,16 @@
-function Product(name, price, tags) {
+function Product(name, price, tags, discount) {
     this.name = ko.observable(name);
     this.price = ko.observable(price);
     tags = typeof(tags) !== 'undefined' ? tags : [];
     this.tags = ko.observableArray(tags);
+
+    discount = typeof(discount) !== 'undefined' ? discount : 0;
+    this.discount = ko.observable(discount);
+
+    this.formatedDiscount = ko.computed(function() {
+        return (this.discount() * 100) + "%";
+    }, this);
+
 };
 
 function PersonViewModel() {
@@ -14,13 +22,13 @@ function PersonViewModel() {
     }, this);
 
     this.shoppingCart = ko.observableArray([
-        new Product("book", 10, ['good', 'low price']),
-        new Product("Home", 250, ['maked with wood']),
-        new Product("Car", 20, ['fast', 'furious'])
+        new Product("book", 10, ['good', 'low price'], .10),
+        new Product("Home", 250, ['maked with wood'], .5),
+        new Product("Car", 20, ['fast', 'furious'], -.3)
     ]);
     this.addProduct = function() {
         var order = this.shoppingCart().length;
-        this.shoppingCart.push(new Product("New Prod", Date(), [1, 2, 3]));
+        this.shoppingCart.push(new Product("New Prod", Date(), [1, 2, 3], order));
     };
 
     this.removeProduct = function(product) {
